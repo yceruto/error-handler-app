06fc647841e327c05515c0da8e2c789aea808228
Yonel Ceruto <yonelceruto@gmail.com>
2019-04-13 22:42:14 -0400

    Add custom error renderer

---
+++ src/ErrorHandler/ErrorRenderer/JsonLdErrorRenderer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\ErrorHandler\ErrorRenderer;
+
+use Symfony\Component\ErrorHandler\ErrorRenderer\ErrorRendererInterface;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+
+class JsonLdErrorRenderer implements ErrorRendererInterface
+{
+    private $debug;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getFormat(): string
+    {
+        return 'jsonld';
+    }
+
+    public function __construct(bool $debug = true)
+    {
+        $this->debug = $debug;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(FlattenException $exception): string
+    {
+        $content = [
+            '@id' => 'https://example.com',
+            '@type' => 'error',
+            '@context' => [
+                'title' => $exception->getTitle(),
+                'code' => $exception->getStatusCode(),
+                'message' => $exception->getMessage(),
+            ],
+        ];
+        if ($this->debug) {
+            $content['@context']['exceptions'] = $exception->toArray();
+        }
+
+        return (string) json_encode($content);
+    }
+}
